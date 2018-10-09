2f06f0dba4257a1b9bdc4a50fc0bd075119abaf4
Daniel Bittencourt <dbittencourt@ermpower.com.au>
2018-10-10 06:57:18 +1100

    code wasnt commit before oO

--- ValidateModelAttribute.cs
+++ ValidateModelAttribute.cs
@@ -1,8 +1,58 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace ModelValidator
 {
-    public class ValidateModelAttribute
+    public class ValidateModelAttribute : ActionFilterAttribute
     {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var descriptor = context.ActionDescriptor as ControllerActionDescriptor;
+
+            if (descriptor != null)
+            {
+                var parameters = descriptor.MethodInfo.GetParameters();
+
+                foreach (var parameter in parameters)
+                {
+                    var argument = context.ActionArguments[parameter.Name];
+
+                    EvaluateValidationAttributes(parameter, argument, context.ModelState);
+                }
+
+                // creates a response with all errors in contained in the 'view state'
+                if (!context.ModelState.IsValid)
+                {
+                    context.Result = new ValidationFailedResult(context.ModelState);
+                }
+            }
+
+            base.OnActionExecuting(context);
+        }
+
+        private void EvaluateValidationAttributes(ParameterInfo parameter, object argument, ModelStateDictionary modelState)
+        {
+            var validationAttributes = parameter.CustomAttributes;
+
+            foreach (var attributeData in validationAttributes)
+            {
+                var attributeInstance = CustomAttributeExtensions.GetCustomAttribute(parameter, attributeData.AttributeType);
+
+                var validationAttribute = attributeInstance as ValidationAttribute;
+
+                if (validationAttribute != null)
+                {
+                    var isValid = validationAttribute.IsValid(argument);
+                    if (!isValid)
+                    {
+                        modelState.AddModelError(parameter.Name, validationAttribute.FormatErrorMessage(parameter.Name));
+                    }
+                }
+            }
+        }
     }
 }
